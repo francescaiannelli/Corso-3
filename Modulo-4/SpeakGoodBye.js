6fa8d0bf082fd48a4f3190922fa01148f5ef7706
Francesca Iannelli <francescaiannelli@MacBook-Air-di-Francesca.local>
2016-07-18 08:44:54 +0200

    new files

---
+++ Modulo-4/SpeakGoodBye.js
@@ -0,0 +1,13 @@
+
+
+(function (window) {
+var byeSpeaker = {}
+var speakWord = "Good Bye";
+byeSpeaker.name = " ";
+byeSpeaker.speak = function () {
+  console.log (speakWord + " " + names);
+}
+
+window.byeSpeaker = byeSpeaker;
+
+})(window);
---
+++ Modulo-4/SpeakHello.js
@@ -0,0 +1,13 @@
+
+
+(function (window) {
+var helloSpeaker = {}
+var speakWord = "Hello";
+helloSpeaker.name = " ";
+helloSpeaker.speak = function () {
+    console.log(speakWord + " " + names);
+}
+  window.helloSpeaker = helloSpeaker;
+
+})(window);
+
